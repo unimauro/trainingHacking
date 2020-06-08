696883854601b88e94dacfe55a22556713abefaf
Unimauo <unimauro@gmail.com>
2020-06-08 05:51:44 -0500

    Repasando Python Finanzas

---
+++ python/2020_Python_for_Finance_Cookbook/00.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import warnings
+plt.style.use('seaborn')
+plt.rcParams['figure.figsize'] = [16, 9]
+plt.rcParams['figure.dpi'] = 300
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
---
+++ python/2020_Python_for_Finance_Cookbook/cp01/cp01.py
@@ -0,0 +1,2 @@
+import pandas as pd
+import yfinance as yf
