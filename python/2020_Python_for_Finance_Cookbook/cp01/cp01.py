61a2a403b4adeb271b8f6d033b5de99d463adb18
Unimauo <unimauro@gmail.com>
2020-06-08 06:13:00 -0500

    Actualizando

--- python/2020_Python_for_Finance_Cookbook/cp01/cp01.py
+++ python/2020_Python_for_Finance_Cookbook/cp01/cp01.py
@@ -1,2 +1,8 @@
 import pandas as pd
+import numpy as np
 import yfinance as yf
+df = yf.download('AAPL', start='2000-01-01', end='2010-12-31', progress=False)
+df = df.loc[:, ['Adj Close']]
+df.rename(columns={'Adj Close':'adj_close'}, inplace=True)
+df['simple_rtn'] = df.adj_close.pct_change()
+df['log_rtn'] = np.log(df.adj_close/df.adj_close.shift(1))
