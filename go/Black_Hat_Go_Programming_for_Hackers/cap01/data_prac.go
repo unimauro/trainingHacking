292ac157216ff431684871a365baa816c0803bd9
Unimauo <unimauro@gmail.com>
2020-08-18 23:35:37 -0500

    Enviando

--- go/Black_Hat_Go_Programming_for_Hackers/cap01/data_prac.go
+++ go/Black_Hat_Go_Programming_for_Hackers/cap01/data_prac.go
@@ -7,3 +7,12 @@ var s = make ([]string,0)
 var m = make(map[string]string)
 s = append(s,"some string")
 m["some key"]="some value"
+
+
+var count := int(42)
+ptr := &count
+fmt.Println(*ptr)
+*ptr=100
+fmt.Println(count)
+
+
